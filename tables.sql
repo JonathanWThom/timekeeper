864d9266e74312ced3570730b496222941740ad1
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-10-13 15:11:44 -0700

    Validates that work block is not before start of the pay period

--- tables.sql
+++ tables.sql
@@ -1,5 +1,13 @@
 CREATE DATABASE timekeeper;
 
+CREATE OR REPLACE FUNCTION ValidStart(started_at TIMESTAMP, pay_period_id INT) RETURNS BOOLEAN AS $$
+	DECLARE started_on DATE;
+    BEGIN
+  		SELECT pay_periods.started_on INTO started_on FROM pay_periods WHERE id=$2;
+        RETURN $1 > started_on;
+    END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY
 );
@@ -30,7 +38,8 @@ CREATE TABLE work_blocks (
   pay_period_id integer NOT NULL REFERENCES pay_periods(id),
   hours numeric NOT NULL,
   started_at timestamp without time zone NOT NULL,
-  ended_at timestamp without time zone NOT NULL
+  ended_at timestamp without time zone NOT NULL,
+  CONSTRAINT valid_start_check CHECK (validstart(started_at, pay_period_id))
 );
 
 CREATE UNIQUE INDEX work_blocks_pkey ON work_blocks(id int4_ops);

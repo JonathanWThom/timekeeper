8e32fc7e845aee093f5ec603a297bb0e537b6f9d
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-10-14 16:22:17 -0700

    Calculates hours correctly

--- models.go
+++ models.go
@@ -19,15 +19,15 @@ type PayPeriod struct {
 
 // WorkBlock lives on the work_blocks table
 type WorkBlock struct {
-	ID          int    `json:"id" db:"id"`
-	ProjectID   int    `json:"project_id" db:"project_id"`
-	PayPeriodID int    `json:"pay_period_id" db:"pay_period_id"`
-	Hours       int    `json:"hours" db:"hours"`
-	StartedAt   string `json:"started_at" db:"started_at"` // store as timestamp in db - should I make it time here?
-	EndedAt     string `json:"ended_at" db:"ended_at"`     // stored as timestamp in db
+	ID          int     `json:"id" db:"id"`
+	ProjectID   int     `json:"project_id" db:"project_id"`
+	PayPeriodID int     `json:"pay_period_id" db:"pay_period_id"`
+	Hours       float64 `json:"hours" db:"hours"`
+	StartedAt   string  `json:"started_at" db:"started_at"` // store as timestamp in db - should I make it time here?
+	EndedAt     string  `json:"ended_at" db:"ended_at"`     // stored as timestamp in db
 }
 
-func (w *WorkBlock) hours() (int, error) {
+func (w *WorkBlock) hours() (float64, error) {
 	layout := "2006-01-02T15:04:05.000Z"
 	end, err := time.Parse(layout, w.EndedAt)
 	if err != nil {
@@ -38,5 +38,5 @@ func (w *WorkBlock) hours() (int, error) {
 		return 0, err
 	}
 
-	return int(end.Sub(start)), nil
+	return float64(end.Sub(start).Hours()), nil
 }
